3b6430afe85fdb637b011add6f05c68e407ea434
Roel <battad09@gmail.com>
2023-10-08 15:32:58 +0800

    added laravel filament

--- resources/views/store/product.blade.php
+++ resources/views/store/product.blade.php
@@ -8,7 +8,6 @@
             <h2 class="text-3xl">{{ $product->name }}</h2>
             <h3 class="text-base">{{ $product->name }}</h3>
             <p class="text-2xl font-bold">$1499.00</p>
-
             <button class="bg-blue-200 border border-blue-700 text-blue-700 p-2 rounded-md font-bold text-sm uppercase">Add to cart</button>
         </div>
     </div>
