b3e8ebd43a0b783c8a354fe2410d466a430a7cdd
Roel <battad09@gmail.com>
2023-10-08 13:44:58 +0800

    add laravel jetstream

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 Route::get('/shop', [App\Http\Controllers\ShopController::class, 'index']);
 Route::get('/cart', [App\Http\Controllers\CartController::class, 'index']);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
